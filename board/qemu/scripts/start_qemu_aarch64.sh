3572d940da306a91ecf612339da83ec06725deed
Rex Nie <niedao516@126.com>
2020-11-10 07:24:33 -0800

    add scripts: copy_from_linux, copy_to_linux ...
    1. getting idea from linux_learning_environment repo, use system.conf as
    config data, let scripts more common
    
    2. creating link point to system_aarch64/x86.conf make sense if want to
    support more arch.
    
    3. after qemu boot, ifconfig eth0 10.0.2.15

--- board/qemu/scripts/start_qemu_aarch64.sh
+++ board/qemu/scripts/start_qemu_aarch64.sh
@@ -1,8 +1,14 @@
+#!/bin/sh
+
+. $(dirname $0)/system.conf
+
+echo "thisdir=$thisdir, topdir=$topdir, buildroot_dir=$buildroot_dir"
+
 sudo qemu-system-aarch64 -M virt \
 	-cpu cortex-a57 -nographic -smp 4 -m 512 \
-	-kernel output/images/Image \
+	-kernel  $linux_kernel \
 	-append "root=/dev/ram0 console=ttyAMA0 kmemleak=on loglevel=8" \
-	-netdev type=tap,ifname=tap0,id=eth0,script=board/qemu/scripts/qemu-ifup_virbr0,queues=2 \
+	-netdev type=tap,ifname=tap0,id=eth0,script=$scripts_dir/qemu-ifup_virbr0,queues=2 \
 	-device virtio-net-pci,netdev=eth0,mac='00:00:00:01:00:01',vectors=6,mq=on \
 #	dyndbg=\"file drivers/of/irq.c +p\"" \
 #	-netdev type=tap,ifname=tap1,id=eth1,script=~/repo/buildroot/board/qemu/scripts/qemu-ifup_virbr0,queues=2 \
